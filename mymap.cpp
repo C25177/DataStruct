8e352e48a9157006638f637f4a47f5eecdb2efaf
C25177 <beloved25177@126.com>
2023-12-28 09:49:57 +0800

    12.28

---
+++ mymap.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <map>
+
+template <typename FIRST, typename SECOND>
+class MapNode {
+private:
+public:
+  FIRST first;
+  SECOND second;
+  MapNode() {}
+  MapNode(SECOND _second, FIRST _first) : second(_second), first(_first) {}
+};
+
+template <typename KEY, typename VALUE>
+class Map {
+  
+};
+
+int main(int argc, char* argv[]) {
+  std::map<int, int> map;
+}
