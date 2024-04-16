77d88ad87bc2040c1ae5cfb4b812e4c19d73d9fa
C25177 <beloved25177@126.com>
2024-04-16 10:32:59 +0800

    4.16

--- mymap.cpp
+++ mymap.cpp
@@ -2,20 +2,23 @@
 #include <map>
 
 template <typename FIRST, typename SECOND>
-class MapNode {
+class MapNode
+{
 private:
 public:
   FIRST first;
   SECOND second;
   MapNode() {}
   MapNode(SECOND _second, FIRST _first) : second(_second), first(_first) {}
+  ~MapNode(SECOND _second, FIRST _first) {}
 };
 
 template <typename KEY, typename VALUE>
-class Map {
-  
+class Map
+{
 };
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
   std::map<int, int> map;
 }
